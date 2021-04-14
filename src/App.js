f2079442ce655b6caa2394a58fb7a8d26a011984
J10v <68559232+J10v@users.noreply.github.com>
2021-04-14 14:18:46 -0700

    Add files via upload

--- src/App.js
+++ src/App.js
@@ -1,16 +1,32 @@
-import About from "./About";
-import "./App.css";
-import Contact from "./Contact";
-import Main from "./Main";
-import Navbar from "./Navbar";
+import './App.css';
+import Footer from'./Footer';
 
 function App() {
   return (
-    <div>
-      <Navbar />
-      <Main />
-      <About />
-      <Contact />
+    <div className="App">
+      <header className="App-header">
+      <section>
+                <header>
+                    <a href="www.google.com" className="logo">Jiovanni</a>
+                    <div className="toggle" onclick="toggleMenu()"></div>
+                    <ul className="nav">
+                        <li><a href="www.google.com">Home</a></li>
+                        <li><a href="www.google.com">About</a></li>
+                    </ul>
+                </header>
+                <div className="content">
+                    <div className="contentBox">
+                        <h2> Hello World </h2>
+                        <p> i am a web developer </p>
+                        <a href="Jiovanni">Contact</a>
+                    </div>
+                </div>
+                <div className="sci">
+                    <li><a href="www.google.com"><i classname="fab fa fa-facebook"></i></a></li>
+                </div>
+            </section>
+      </header>
+      <Footer />
     </div>
   );
 }
