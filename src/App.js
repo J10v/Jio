0f9d8ec76320caa310e9c59ccfb6143a34afe20a
J10v <68559232+J10v@users.noreply.github.com>
2021-04-14 16:27:29 -0700

    Add files via upload

--- src/App.js
+++ src/App.js
@@ -1,32 +1,16 @@
-import './App.css';
-import Footer from'./Footer';
+import About from "./About";
+import "./App.css";
+import Contact from "./Contact";
+import Main from "./Main";
+import Navbar from "./Navbar";
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-      <section>
-                <header>
-                    <a href="www.google.com" className="logo">Jiovanni</a>
-                    <div className="toggle" onclick="toggleMenu()"></div>
-                    <ul className="nav">
-                        <li><a href="www.google.com">Home</a></li>
-                        <li><a href="www.google.com">About</a></li>
-                    </ul>
-                </header>
-                <div className="content">
-                    <div className="contentBox">
-                        <h2> Hello World </h2>
-                        <p> i am a web developer </p>
-                        <a href="Jiovanni">Contact</a>
-                    </div>
-                </div>
-                <div className="sci">
-                    <li><a href="www.google.com"><i classname="fab fa fa-facebook"></i></a></li>
-                </div>
-            </section>
-      </header>
-      <Footer />
+    <div>
+      <Navbar />
+      <Main />
+      <About />
+      <Contact />
     </div>
   );
 }
--- src/Contact.js
+++ src/Contact.js
@@ -1,46 +1,29 @@
-import React from 'react'
-
-import React from 'react'
-
+import React from "react";
+import { FaEnvelope, FaGithub, FaLinkedinIn } from "react-icons/fa";
+import "./Contact.css";
+import "bootstrap-social/bootstrap-social.css";
 function Contact() {
-    return (
-        <div className="contact-section">
-            
+  return (
+    <div className="contact-section">
+      <div className="col-6 col-sm-3 text-center">
+        <h1 class="title">Contact</h1>
+        <hr />
+      </div>
+      <div className="container">
+        <br />
+        <FaEnvelope href="jiovanni@gmail.com" className="envelope" />{" "}
+<div>
+          <a href="jiovanni554@gmail.com"> Jiovanni554@gmail.com</a>
+         
         </div>
-    )
+        <FaLinkedinIn className="envelope" /> 
+
+        <FaGithub className="envelope" />
+
+        
+      </div>
+    </div>
+  );
 }
 
-export default Contact
-
-
-
-
-import React, { Component } from 'react';
-import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
-import {library} from '@fortawesome/fontawesome-svg-core';
-import { faGithub, faLinkedin } from '@fortawesome/free-brands-svg-icons';
-
-function Footer(props) {
-    return (
-        <footer className="site-footer">
-            <div className="container">
-                <div className="row">             
-                    
-                    <div className="col-6 col-sm-3 text-center">
-                        <h5>Contact</h5>
-                        <FontAwesomeIcon icon={faGithub} />
-                        <FontAwesomeIcon icon={faLinkedin} /> 
-                        <a className="btn btn-social-icon btn-twitter" href="http://twitter.com/"><i className="fa fa-twitter" /></a>{' '}
-                        <a className="btn btn-social-icon btn-google" href="http://youtube.com/"><i className="fa fa-youtube" /></a> 
-                    </div>
-                    <div className="col-sm-4 text-center">
-                        <a role="button" className="btn btn-link" href="tel:+14083944725"><i className="fa fa-phone" /> 1(408)394-4725</a><br />
-                        <a role="button" className="btn btn-link" href="mailto:notreal@notreal.co"><i className="fa fa-envelope-o" /> j_sanchez@ymail.com</a>
-                    </div>
-                </div>
-            </div>
-        </footer>
-    );
-}
-
-export default Footer;
+export default Contact;
